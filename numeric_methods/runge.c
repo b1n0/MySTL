b1335efd9592c8552bfba57e7af393e835820270
b1n0 <azim4fs@gmail.com>
2019-09-14 20:58:11 +0300

    fix

--- numeric_methods/runge.c
+++ numeric_methods/runge.c
@@ -2,23 +2,22 @@
 
 #define sum(y, k, c) for(i = 0; i < size; i++) buff[i] = (y)[i] + (c)*(k)[i] 
 #define ST_SIZE 32
-#define eps 0.00000001
 
 int evaluate(double x0, double x, double* y0, double *y, int size, int num_steps) {
-	int i = 0;
+	int i = 0, j = 0;
 	double h = (x - x0)/num_steps, k[4*ST_SIZE], buff[ST_SIZE];
 	memcpy(y, y0, size*sizeof(double)); 
-	for (; x0 < x + eps - h; x0 += h) {
+	for (j = 0; j < num_steps; j++) {
 		f(x0, y, size, k);
 		sum(y, k, h/2);
 		f(x0 + h/2, buff, size, k + size);
 		sum(y, k + size, h/2);
 		f(x0 + h/2, buff, size, k + 2*size);
 		sum(y, k + 2*size, h);
-		f(x0 + h, buff, size, k + 3*size);	
-	
+		x0 += h;
+		f(x0, buff, size, k + 3*size);	
 		for(i = 0; i < size; i++) 
-			y[i] = y[i] + (k[i] + 2*k[size + i] + 2*k[2*size + i] + k[3*size + i])*h/6;
+			y[i] = y[i] + (k[i] + 2.*k[size + i] + 2.*k[2*size + i] + k[3*size + i])*h/6.;
 	}
 	return 0;
 }
