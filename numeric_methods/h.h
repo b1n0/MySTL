52be62312e1a26a9d961c0ff5f21e037b9f5e7c2
b1n0 <azim4fs@gmail.com>
2019-09-15 15:24:39 +0300

    fix

--- numeric_methods/h.h
+++ numeric_methods/h.h
@@ -9,3 +9,5 @@ void f(double x, double* y, int size, double* res);
 int runge(double x0, double x, double *y0, double *y, int size, int num_steps);
 int runge_with_autostep(double x0, double x, double* y0, double* y, int size, double h, double err, double K);
 int euler(double x0, double x, double* y0, double* y, int size, int num_steps);
+void print(double* y, int size);
+int plot(double* y, int num_points);
--- numeric_methods/main.c
+++ numeric_methods/main.c
@@ -1,16 +1,19 @@
 #include "h.h"
+#define NUM_POINTS 100
 
 void f(double x, double* y, int size, double* res) {
-	int i;
-	for(i = 0; i < size; i++) res[i] = x;
+	res[0] = y[1];
+	res[1] = -1.*y[0];
 }
-
+		
 int main(void) {
 	int i;
-	double x0 = 0, x = 5, y0[1], y[1];
-	y0[0] = 0; 
-	//runge_with_autostep(x0, x, y0, y, 1, 0.02, 0.00000001, -1);	
-	euler(x0, x, y0, y, 1, 100);
-	printf("y =  %lf\n", y[0]);
+	double x0 = 0, x = 0, y0[2], y[2*NUM_POINTS], h = 0.1;
+	y0[0] = 1; y0[1] = 0; 
+	for(i = 0; i < NUM_POINTS; i++, x+=h) 
+		runge_with_autostep(x0, x, y0, y + 2*i, 2, 0.02, 0.00000001, -1);
+		//euler(x0, x, y0, y + 2*i, 2, 1000);
+		//runge(x0, x, y0, y0 + 2*i, 2, 1000);
+	plot(y, NUM_POINTS);	
 	return 0;
 }
