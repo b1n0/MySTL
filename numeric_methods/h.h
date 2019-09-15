d476a28a5277fff1b86a6bb610c930096028c2f7
b1n0 <azim4fs@gmail.com>
2019-09-15 11:15:24 +0300

    fix

--- numeric_methods/h.h
+++ numeric_methods/h.h
@@ -6,6 +6,6 @@
 #include <math.h>
 
 void f(double x, double* y, int size, double* res);
-int evaluate(double x0, double x, double *y0, double *y, int size, int num_steps);
-int evaluate_with_autostep(double x0, double x, double* y0, double* y, int size, double h, double err, double K);
-
+int runge(double x0, double x, double *y0, double *y, int size, int num_steps);
+int runge_with_autostep(double x0, double x, double* y0, double* y, int size, double h, double err, double K);
+int euler(double x0, double x, double* y0, double* y, int size, int num_steps);
