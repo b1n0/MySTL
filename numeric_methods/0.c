4d00ac19633f68c5c0fd606874089d9e78a62bf8
b1n0 <azim4fs@gmail.com>
2019-09-21 15:59:28 +0300

    fix

--- numeric_methods/0.c
+++ numeric_methods/0.c
@@ -1,29 +1,3 @@
-<<<<<<< HEAD
-#include "h.h"
-
-int derivative(double x, double* y, double* res, int size, double h) {
-	int i;
-	double buff[32];
-	f(x, y, size, res);
-	f(x + h, y, size, buff);
-	for(i = 0; i < size; i++)
-		res[i] = (buff[i] - res[i])/h;
-	return 0;
-}
-void f(double x, double* y, int size, double* res) {
-       res[0] = (exp(2.*x) - 1)/(exp(2.*x) + 1);
-}       
-
-int main(void) {
-	int i;
-	double y[1], res[1];
-	derivative(0, y, res, 1, 0.01);	
-	print(res,1);
-	return 0;
-}
-
-
-=======
 #include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
@@ -38,4 +12,3 @@ int main(void) {
 	}
 	return 0;
 }
->>>>>>> a68f61229313779aff5f5379be5325224cd15ec0
