128fe5dd7fc2b5330076e57f7147afa0c1711608
b1n0 <azim4fs@gmail.com>
2019-09-21 21:27:10 +0300

    fix

--- numeric_methods/algorithms.c
+++ numeric_methods/algorithms.c
@@ -5,19 +5,23 @@
 
 int runge(double x0, double x, double* y0, double *y, int size, int num_steps) {
 	int i = 0, j = 0;
-	double h = (x - x0)/num_steps, k[4*ST_SIZE], buff[ST_SIZE];
+	double h = (x - x0)/num_steps, k[6*ST_SIZE], buff[ST_SIZE];
 	memcpy(y, y0, size*sizeof(double)); 
 	for (j = 0; j < num_steps; j++) {
 		f(x0, y, size, k);
-		sum(y, k, h*0.5);
+		for(i = 0; i < size; i++) buff[i] = y[i] + h*k[i]*0.5;
 		f(x0 + h*0.5, buff, size, k + size);
-		sum(y, k + size, h/2);
+		for(i = 0; i < size; i++) buff[i] = y[i] + h*(k[i] + k[size+i])*0.25;
 		f(x0 + h*0.5, buff, size, k + 2*size);
-		sum(y, k + 2*size, h);
-		x0 += h;
-		f(x0, buff, size, k + 3*size);	
+		for(i = 0; i < size; i++) buff[i] = y[i] + h*(2*k[2*size+i] - k[size+i]);
+		f(x0 + h, buff, size, k + 3*size);	
+		for(i = 0; i < size; i++) buff[i] = y[i] + h*(7*k[i] +10*k[size + i] + k[3*size + i])/27;
+		f(x0 + 2*h/3, buff, size, k + 4*size);
+		for(i = 0; i < size; i++) buff[i] = y[i] + h*(28*k[i] - 125*k[size+i] + 546*k[2*size+i] + 54*k[3*size+i] - 378*k[4*size+i])/625;
+		f(x0 + h*0.2, buff, size, k + 5*size);
+		x0 += h;	
 		for(i = 0; i < size; i++) 
-			y[i] = y[i] + (k[i] + 2.*k[size + i] + 2.*k[2*size + i] + k[3*size + i])*h/6.;
+			y[i] += (14*k[i] + 35*k[3*size+i] + 162*k[4*size+i] + 125*k[5*size+i])*h/336;
 	}
 	return 0;
 }
@@ -28,22 +32,32 @@ int runge_with_autostep(double x0, double x, double* y0, double* y, int size, do
 	memcpy(y, y0, size * sizeof(double));
 	for(; x0 < x - h; x0 += h) {
 		while (1) {
+			double c;
 			f(x0, y, size, k);
-			sum(y, k, h/2);
+			for(i = 0; i < size; i++) buff[i] = y[i] + h*k[i]*0.5;
 			f(x0 + h*0.5, buff, size, k + size);
-			sum(y, k + size, h/2);
+			for(i = 0; i < size; i++) buff[i] = y[i] + h*(k[i] + k[size+i])*0.25;
 			f(x0 + h*0.5, buff, size, k + 2*size);
-			sum(y, k + 2*size, h);
-			f(x0 + h, buff, size, k + 3*size);
-			for(E = 0, i = 0; i < size; i++)
-				E +=(k[i] - k[size + i] - k[2*size + i] + k[3*size + i]) * 
-					(k[i] - k[size + i] - k[2*size + i] + k[3*size + i]) * 4./9.;
-			if (E < err/K)  h *= 2;
-			else if (E > err)  h *= 0.5; 
+			for(i = 0; i < size; i++) buff[i] = y[i] + h*(2*k[2*size+i] - k[size+i]);
+			f(x0 + h, buff, size, k + 3*size);	
+			for(i = 0; i < size; i++) buff[i] = y[i] + h*(7*k[i] +10*k[size + i] + k[3*size + i])/27;
+			f(x0 + 2*h/3, buff, size, k + 4*size);
+			for(i = 0; i < size; i++) buff[i] = y[i] + h*(28*k[i] - 125*k[size+i] + 546*k[2*size+i] + 54*k[3*size+i] - 378*k[4*size+i])/625;
+			f(x0 + h*0.2, buff, size, k + 5*size);
+
+			for(E = 0, i = 0; i < size; i++) {
+				c = ((-42)*k[i] - 244*k[2*size+i] - 21*k[3*size+i] + 162*k[4*size+i] + 125*k[5*size+i])*h/336;
+				E += c*c;
+			}
+			if (E < err/K) {
+			       	h *= 2;
+				break;
+			}
+			else if (E > err) h *= 0.5; 
 			else break;
 		}
 		for(i = 0; i < size; i++)
-			y[i] += (k[i] + 2.*k[size + i] + 2.*k[2*size + i] + k[3*size + i])*h/6.;
+			y[i] += (14*k[i] + 35*k[3*size+i] + 162*k[4*size+i] + 125*k[5*size+i])*h/336;
 	}
 	runge(x0, x, y, buff, size, 20);
 	memcpy(y, buff, size * sizeof(double));
