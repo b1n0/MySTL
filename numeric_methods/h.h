569d679c67774cf56934ace4b7cb5b021bc62367
b1n0 <azim4fs@gmail.com>
2019-09-22 11:59:25 +0300

    fix

--- numeric_methods/h.h
+++ numeric_methods/h.h
@@ -6,9 +6,8 @@
 #include <math.h>
 
 void f(double x, double* y, int size, double* res);
+int euler(double x0, double x, double* y0, double* y, int size, int num_steps);
 int runge(double x0, double x, double *y0, double *y, int size, int num_steps);
 int runge_with_autostep(double x0, double x, double* y0, double* y, int size, double h, double err, double K);
-int euler(double x0, double x, double* y0, double* y, int size, int num_steps);
 void print(double* y, int size);
 int plot(double* y, int num_points);
-int derivative(double x, double* y, double* res, int size, double h);
