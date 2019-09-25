aade5fc85f75731b7d752840a141fcd06c9d4e4b
b1n0 <azim4fs@gmail.com>
2019-09-25 21:25:53 +0300

    fix

--- numeric_methods/main.c
+++ numeric_methods/main.c
@@ -2,27 +2,13 @@
 #define NUM_POINTS 100
 
 void f(double x, double* y, int size, double* res) {
-	res[0] = 2*x; //y[1];
-	res[1] = 2*x; //-1.*y[0];
+	res[0] = y[1] - y[0];
+	res[1] = y[1] - y[0] - x/y[0];
 }
-/*		
-int main(void) {
-	int i;
-	double x0 = 0, x = 0, y0[2], y[2*NUM_POINTS], h = 0.1;
-	y0[0] = 1; y0[1] = 0; 
-	for(i = 0; i < NUM_POINTS; i++, x+=h) 
-		//euler(x0, x, y0, y + 2*i, 2, 10000);
-		runge(x0, x, y0, y0 + 2*i, 2, 100);
-		//runge_with_autostep(x0, x, y0, y + 2*i, 2, 0.02, 0.00000001, 10);
-	plot(y, NUM_POINTS);	
-	return 0;
-}
-*/
 
 int main(void) {
-	int i, k = 1;
-	double x0 = 0, x = 0, y0[2];
-	y0[0] = 0; y0[1] = 1;
-        shooting(y0, 2, 1, 0, 1);
+	double y0[5];
+	y0[0] = 0.; y0[1] = 0;
+        shooting(y0, 2, 1, 0., 1.);
 	return 0;	
 }
--- numeric_methods/shooting.c
+++ numeric_methods/shooting.c
@@ -3,6 +3,7 @@
 #define delta 0.00000001
 #define ST_SIZE 32
 #define NUM_STEPS 1000
+#define NUM_SHOTS 1
 #define random(min, max) (min) + ((double)rand()/RAND_MAX)*((max) - (min))
 
 int fill_random(double* beta, int k) {
@@ -15,31 +16,31 @@ int shooting(double* y0, int size, int k, double a, double b) {
 	int i, j;
 
 	m = (double**)malloc((size-k)*sizeof(double*));
-	for(i = 0; i < size; i++) m[i] = malloc(size*sizeof(double));
-
+	for(i = 0; i < size - k; i++) m[i] = malloc(size*sizeof(double));
 	fill_random(y0_buff + k, size - k);
 	memcpy(y0_buff, y0, k*sizeof(double));
 	
-	for(int shot = 0; shot < 10; shot++) {
-		runge(a, b, y0_buff, res, size, NUM_STEPS);
+	for(int shot = 0; shot < NUM_SHOTS; shot++) {
+		//runge(a, b, y0_buff, res, size, NUM_STEPS);
+		runge_with_autostep(a, b, y0_buff, res, size, 0.0001, 0.0000001, 10);
 		for(i = 0; i < size - k; i++) {
 			if (i > 0) 
 		       		y0_buff[k + i - 1] -= delta; 
 			y0_buff[k + i] += delta;
-			runge(a, b, y0_buff, m[i], size, NUM_STEPS);
+			//runge(a, b, y0_buff, m[i], size, NUM_STEPS);
+			runge_with_autostep(a, b, y0_buff, m[i], size, 0.0001, 0.000000001, 10);
 			for(j = 0; j < size - k; j++) 
 				m[i][k+j] = (m[i][k+j] - res[k+j])/delta;
 			v[i] = y0[k+i] - res[k+i];
 		}
+		//print(res, size);
 		y0_buff[size-1] -= delta;
-		gauss(m, h, v, k);
+		gauss(m, h, v, size - k);
 		for(i = 0; i < size - k; i++) 
 			y0_buff[k+i] += h[i];
+		print(v, size - k);
 	}
-	runge(a, 0.5, y0_buff, res, size, 100);	
-	print(res, 2);
-
-	for(i = 0; i < size; i++) free(m[i]);
+	for(i = 0; i < size - k; i++) free(m[i]);
 	free(m);
 	return 0;
 }
