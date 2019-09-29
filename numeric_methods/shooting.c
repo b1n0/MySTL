5dcad4f8c10194ebd9f59eec49a98aafc121a0f1
b1n0 <azim4fs@gmail.com>
2019-09-29 19:46:40 +0300

    fix

--- numeric_methods/shooting.c
+++ numeric_methods/shooting.c
@@ -6,7 +6,7 @@
 #define random(min, max) (min) + ((double)rand()/RAND_MAX)*((max) - (min))
 
 int shooting(double* y0, int size, int k, double a, double b) {
-	double **m, y0_buff[ST_SIZE], res[ST_SIZE], v[ST_SIZE], h[ST_SIZE], x, err, c = 1.;
+	double **m, y0_buff[ST_SIZE], res[ST_SIZE], v[ST_SIZE], h[ST_SIZE], x, err;
 	int i, j;
 
 	m = create_matrix(size - k, size);
@@ -15,7 +15,7 @@ int shooting(double* y0, int size, int k, double a, double b) {
 	memcpy(y0_buff, y0, k*sizeof(double));
 	
 	while(1) {
-		runge_with_autostep(a, b, y0_buff, res, size, 0.0001, 0.01);
+		runge(a, b, y0_buff, res, size, 1000);
 		for(i = 0 ; i < size - k; i++)
 			v[i] = y0[k+i] - res[k+i];
 		err = norm(v, size - k);
@@ -26,15 +26,14 @@ int shooting(double* y0, int size, int k, double a, double b) {
 				if (i > 0) 
 		       			y0_buff[k + i - 1] -= delta; 
 				y0_buff[k + i] += delta;
-				runge_with_autostep(a, b, y0_buff, m[i], size, 0.0001, 0.01);
+				runge(a, b, y0_buff, m[i], size, 1000);
 				for(j = 0; j < size - k; j++) 
 					m[i][k+j] = (m[i][k+j] - res[k+j])/delta;
 			}
 			y0_buff[size-1] -= delta;
 			gauss(m, h, v, size - k);
 			for(i = 0; i < size - k; i++) 
-				y0_buff[k+i] += c*h[i];
-			c*=0.5;
+				y0_buff[k+i] += h[i];
 		}
 		else break;
 	}
