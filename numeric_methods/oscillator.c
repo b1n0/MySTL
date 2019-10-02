2d70621bc2cb448ff1ac3f6174ee382475547738
b1n0 <azim4fs@gmail.com>
2019-10-02 21:49:48 +0300

    fix

--- numeric_methods/oscillator.c
+++ numeric_methods/oscillator.c
@@ -13,6 +13,7 @@ void plot(char* fname, int num_points) {
 
 int main(void) {
 	double x0 = 0, x = 0, y0[2], y[2], h = 2*PI/NUM_POINTS;
+	double y8[2], y10[2], y12[2];
 	y0[0] = 0; y0[1] = 1;
 	FILE *out = fopen("data.txt", "w");
 	for(int i = 0; i < NUM_POINTS; i++, x+=h) {
@@ -20,6 +21,10 @@ int main(void) {
 		runge_with_autostep(x0, x, y0, y, 2, 1.e-9, 1.e-8);
 		fprintf(out, "%lf %lf \n", y[0], y[1]);
 	}
+	runge_with_autostep(x0, PI, y0, y8, 2, 1.e-9, 1.e-8);
+	runge_with_autostep(x0, PI, y0, y10, 2, 1.e-11, 1.e-10);
+	runge_with_autostep(x0, PI, y0, y12, 2, 1.e-13, 1.e-12);
+	printf("%lf %lf \n", (y8[0] - y10[0])/(y10[0] - y12[0]), (y8[1] - y10[1])/(y10[1] - y12[1]));
 	plot("data.txt", NUM_POINTS);
 	fclose(out);
 	return 0;
