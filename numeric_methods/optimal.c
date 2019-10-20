adc189a64f1a69f483c4bd5232109261988c29e3
b1n0 <azim4fs@gmail.com>
2019-10-20 15:28:42 +0300

    fix

--- numeric_methods/optimal.c
+++ numeric_methods/optimal.c
@@ -6,10 +6,10 @@ double u(double* y);
 double u(double* y) { return y[3] > 0 ? 2. : -2.; }
 
 void f(double x, double* y, int size, double* res) {
-	res[0] = u(y);
-	res[1] = y[0];
-	res[2] = -BETA*pow(y[1], BETA - 1)/(1 + exp(ALPHA*y[0]*y[0]));
-	res[3] = -1*y[2] + 2*ALPHA*pow(y[1], BETA)*y[0]*exp(ALPHA*y[0]*y[0])/pow(1+exp(ALPHA*y[0]*y[0]), 2);
+	res[0] = u(y); // y
+	res[1] = y[0]; // x
+	res[2] = -BETA*pow(y[1], BETA - 1)/(1 + exp(ALPHA*y[0]*y[0])); //px
+	res[3] = -1*y[2] + 2*ALPHA*pow(y[1], BETA)*y[0]*exp(ALPHA*y[0]*y[0])/pow(1+exp(ALPHA*y[0]*y[0]), 2); //py
 }
 
 double eigen_value(double x, double* y) { return 0; }
@@ -20,23 +20,11 @@ void discrepancy(double* y0, double* y, double* v) {
 	v[2] = y[2] + y0[2];	
 }
 
-void jacobian(double** m, double* y0, double* v) {
-	double y[4], dv[3];
-	for(int i = 0; i < 3; i++) {
-		y0[1+i] += DELTA;
-		runge_hardcore(0, 4, y0, y, 4, 1.e-8, 1.e-7);
-		y0[1+i] -= DELTA;
-		discrepancy(y0, y, dv);
-		m[0][i] = (dv[0] - v[0])/DELTA;
-		m[1][i] = (dv[1] - v[1])/DELTA;
-		m[2][i] = (dv[2] - v[2])/DELTA;
-	}
-}
-
 int main(void) {
 	double a, b, y0[4], y[4];
+	a = 0.; b = 4.;
 	y0[0] = 0;
-	shoot(a, b, y0, 4, 1, 0.0001);
+	shoot(a, b, y0, 4, 1, 0.000001);
 	printf("%lf %lf %lf %lf \n", y0[0], y0[1], y0[2], y0[3]);	
 	return 0;
 }
--- numeric_methods/shooting.c
+++ numeric_methods/shooting.c
@@ -1,56 +1,22 @@
 #include "h.h"
 
-int shooting(double a, double b, double* y0, int size, int k, double eps) {
-	double **m, *A, y0_buff[ST_SIZE], y[ST_SIZE], v[ST_SIZE], h[ST_SIZE], err;
-	int i, j;
-	m = (double**)malloc((size - k)*sizeof(double*));
-	A = (double*)malloc((size - k)*size*sizeof(double));
-	for(i = 0; i < size - k; i++) m[i] = m[i] = A + i*size + k;
-	for(i = 0; i < size - k; i++) y0_buff[k+i] = 1.;
-	memcpy(y0_buff, y0, k*sizeof(double));
-	
-	while(1) {
-		runge_hardcore(a, b, y0_buff, y, size, 1.e-7, 1.e-6);
-		for(i = 0 ; i < size - k; i++)
-			v[i] = y0[k+i] - y[k+i];
-		err = norm(v, size - k, 'm');
-		printf("%lf \n", err);
-		if(err > eps) {
-			for(i = 0; i < size - k; i++) {
-				if (i > 0) 
-		       			y0_buff[k + i - 1] -= DELTA; 
-				y0_buff[k + i] += DELTA;
-				runge_hardcore(a, b, y0_buff, A + i*size, size, 1.e-7, 1.e-6);
-				for(j = 0; j < size - k; j++) 
-					m[i][j] = (m[i][j] - y[k+j])/DELTA;
-			}
-			y0_buff[size-1] -= DELTA;
-			gauss(m, h, v, size - k);
-			for(i = 0; i < size - k; i++) y0_buff[k+i] += h[i];
-		}
-		else break;
-	}
-	free(A); free(m);
-	memcpy(y0, y0_buff, size*sizeof(double));
-	return 0;
-}
-
 int shoot(double a, double b, double* y0, int size, int k, double eps) {
 	int i, j, n, flag;
 	double **m, y0_buff[ST_SIZE], v[ST_SIZE], y[ST_SIZE], h[ST_SIZE];
 	double err, prev_err, c;
-	prev_err = 5.;
 	m = create_matrix(size - k, size - k);
-	for(i = k; i < size; i++) y0[i] = 1.;
+	for(i = k; i < size; i++) y0[i] = 3.;
 	//add loop for different beta
 	//add return 0 condition
 	y0_buff[0] = y0[0];
-	runge_hardcore(a, b, y0, y, size, 1.e-8, 1.e-7);
+	memcpy(y0_buff, y0, sizeof(double)*size);
+	runge_hardcore(a, b, y0, y, size, 1.e-11, 1.e-10);
 	discrepancy(y0, y, v);
-	for(err = norm(v, size - k, 'm'); err > eps; prev_err = err) {
+	for(err = norm(v, size - k, 'm'), prev_err = err; err > eps; prev_err = err) {
+		printf("%lf \n", err);
 		for(i = k; i < size; i++) {
 			y0[i] += DELTA;
-			runge_hardcore(a, b, y0, y, size, 1.e-8, 1.e-7);
+			runge_hardcore(a, b, y0, y, size, 1.e-11, 1.e-10);
 			discrepancy(y0, y, h);
 			for(j = 0; j < size - k; j++) m[j][i-k] = (h[j] - v[j])/DELTA;
 			y0[i] -= DELTA;
@@ -58,7 +24,7 @@ int shoot(double a, double b, double* y0, int size, int k, double eps) {
 		gauss(m, h, v, size - k);
 		for(c = 1., flag = 1, i = 0; flag == 1 && i < 30 ; i++, c*=0.5 ) {
 			for(j = k; j < size; j++) y0_buff[j] = y0[j] - c*h[j - k];	
-			runge_hardcore(a, b, y0_buff, y, size, 1.e-8, 1.e-7);
+			runge_hardcore(a, b, y0_buff, y, size, 1.e-11, 1.e-10);
 			discrepancy(y0, y, v);
 			err = norm(v, size - k, 'm');
 			if(err < prev_err) flag = 0;
