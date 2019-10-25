25087030d3d02a6d628d7e895672583f9349f30c
b1n0 <azim4fs@gmail.com>
2019-10-25 09:26:41 +0300

    fix

--- numeric_methods/optimal.c
+++ numeric_methods/optimal.c
@@ -8,10 +8,9 @@ double u(double* y) { return y[3] > 0 ? 2. : -2.; }
 void f(double x, double* y, int size, double* res) {
 	res[0] = u(y); // y
 	res[1] = y[0]; // x
-	res[2] = -BETA*pow(y[1], BETA - 1)/(1 + exp(ALPHA*y[0]*y[0])); //px
-	res[3] = -1*y[2] + 2*ALPHA*pow(y[1], BETA)*y[0]*exp(ALPHA*y[0]*y[0])/pow(1+exp(ALPHA*y[0]*y[0]), 2); //py
+	res[2] = BETA*pow(y[1], BETA - 1)/(1 + exp(ALPHA*y[0]*y[0])); //px
+	res[3] = -1*y[2] - 2*ALPHA*pow(y[1], BETA)*y[0]*exp(ALPHA*y[0]*y[0])/pow(1+exp(ALPHA*y[0]*y[0]), 2); //py
 }
-
 double eigen_value(double x, double* y) { return 0; }
 
 void discrepancy(double* y0, double* y, double* v) { 
@@ -23,7 +22,7 @@ void discrepancy(double* y0, double* y, double* v) {
 int main(void) {
 	double a, b, y0[4];
 	a = 0.; b = 4.;
-	y0[0] = 0;
+	y0[0] = 0; 
 	shoot(a, b, y0, 4, 1, 0.0001);
 	printf("%lf %lf %lf %lf \n", y0[0], y0[1], y0[2], y0[3]);	
 	plot(a, b, y0, 4, 100);
