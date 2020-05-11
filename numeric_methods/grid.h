05b49fc8ed7a757d3e3a05623bb7d2dcf9bb5d62
b1n0 <azim4fs@gmail.com>
2020-05-11 12:16:26 +0300

    fix

--- numeric_methods/grid.h
+++ numeric_methods/grid.h
@@ -15,6 +15,15 @@ double norm(double* y, int size, const char name);
 int multiply(double** A, double** B, double** res, int arows, int acols, int brows, int bcols);
 void triangle(double** m, double* b, int n);
 void gauss(double** m, double* x, double* b, int n);
+void running_method(int n, double* x, double** c, double* f);
+
+double** create_matrix(int rows, int cols);
+void delete_matrix(double** A, int rows);
+void explicit_solve(double** u, int tn, int xn, double a);
+void implicit_solve(double** u, int tn, int xn, double a);
+void inaccuracy(double* u, double* v, int xn, double a, double* ans);
+void save(double** u, int tn, int xn, const char* fname);
+void plot(double** u, int tn, int xn, const char* fname);
 
 #define MAX(a,b) (a)>(b)?(a):(b)
 #define MIN(a,b) (a)<(b)?(a):(b)
--- numeric_methods/gridfunctions.c
+++ numeric_methods/gridfunctions.c
@@ -1,7 +1,4 @@
-#include "h.h"
-
-#define Tn 10001
-#define Xn 10001
+#include "grid.h"
 
 void explicit_solve(double** u, int tn, int xn, double a) {
 	double x, t, h = 1./(xn-1), tau = 1./(tn-1);
@@ -78,25 +75,5 @@ void plot(double** u, int tn, int xn, const char* fname) {
 	fclose(f);
         fprintf(gnuplot_pipe, "%s%s%s\n", "set key off; plot for[col=2:5:1]'", fname, "' using 1:col with lines");
 	fclose(gnuplot_pipe);
-}
-
-int main(void) {
-	int i, j, k, xn, tn;
-	double ans[4], **u, **v, a;
-	v = create_matrix(Tn, Xn);
-	for(k = 0, a = 1.; k < 2; k++, a = 0., printf("\n")) {
-		implicit_solve(v, Tn, Xn, a);
-		plot(v, Tn, Xn, "plot.txt");
-		for(i = 0, tn = 11; i < 3; i++, tn = (tn-1)*10 + 1)
-			for(j = 0, xn = 11; j < 3; j++, xn = (xn-1)*10 + 1) { 
-				u = create_matrix(tn, xn);
-				implicit_solve(u, tn, xn, a);
-				inaccuracy(u[tn-1], v[Tn-1], xn, a, ans);
-				printf("%lf| %lf| %lf %lf %lf %lf \n",1./(tn-1), 1./(xn-1), ans[0], ans[1], ans[2], ans[3]);
-				delete_matrix(u, tn);
-			}	
-	}
-	delete_matrix(v, Tn);
-	return 0;
 }
 
